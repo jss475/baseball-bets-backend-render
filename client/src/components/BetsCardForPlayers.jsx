70a75658a3dce9de518359142d5b2f8554da5fc8
JosephShin <joespam9999@gmail.com>
2022-06-22 17:50:58 -0400

    formatted BetsCardForPlayers

--- client/src/components/BetsCardForPlayers.jsx
+++ client/src/components/BetsCardForPlayers.jsx
@@ -7,9 +7,15 @@ function BetsCardForPlayers({bet}){
     
     return (
         <>
-            <p>Description: {description}</p>
-            <p>Odds : {odds}</p>
-            <p>Current bets: ${current_bets}</p>
+            <div className="card border-dark mb-3 text-center" style = {{width: "30rem"}}>
+                <div className ="card-body">
+                    <p className="card-text">{description}</p>
+                </div>
+                <div className ="card-body">
+                    <h3><span className = "badge bg-pill bg-primary">Odds: {odds}</span></h3>
+                    <h3><span className = "badge bg-pill bg-primary">Current bets: ${current_bets}</span></h3>
+                </div>
+            </div> 
         </>
     )
 }
