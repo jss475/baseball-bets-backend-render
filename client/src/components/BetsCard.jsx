6ccf713ac07df87c65351a0b42ef288e6f063dd1
JosephShin <joespam9999@gmail.com>
2022-06-23 15:28:25 -0400

    bets cards css added

--- client/src/components/BetsCard.jsx
+++ client/src/components/BetsCard.jsx
@@ -1,5 +1,7 @@
 import { useHistory, useLocation } from 'react-router-dom'
 import BetsForm from './BetsForm'
+import '../bets_card.css'
+
 
 function BetsCard({ bet, show, handleSetShow, handleAddBet, handleDeleteBet}){
     //refactor the bet prop for the properties needed
@@ -20,21 +22,104 @@ function BetsCard({ bet, show, handleSetShow, handleAddBet, handleDeleteBet}){
     
     return (
         <>
-            <p>Player: {player.name}</p>
-            <p>Team: {player.team_name}</p>
-            <img src={player.image} alt = "hello"/>
-            <p>Description: {description}</p>
-            <p>Odds: {odds}</p>
-            <p>Current bets: ${current_bets}</p>
-            <p>Did the bet win? {win ? "Yes!" : "No!"}</p>
+          <div className="bets-card">  
 
-            {!show ? <button onClick={handleBetClick}>Place Bet</button> : null}
-            {show ? <BetsForm bet={bet}
-                    handleAddBet={handleAddBet}
-                    handleDeleteBet={handleDeleteBet}
-            /> : null}
+            <figure className="bets-card-water">
+              <div className="bets-card-img-container">
+                <img className="bets-img" src={player.image} alt = "hello"/>
+              </div>
+              
+              <figcaption className="bets-card-caption">
+                <h1 className="bets-card-name">{player.name}</h1>
+
+                <p>{description}</p>
+
+
+                <div className="bets-card-money">
+                  <h4 className="bets-card-money-child">
+                    <span className="bets-card-label">Odds</span>
+                    {odds}
+                  </h4>
+                  <h4 className="bets-card-money-child">
+                    <span className="bets-card-label">Current Bets</span>
+                    ${current_bets}
+                  </h4>
+                </div>
+              </figcaption>
+              <div className="bets-card-btn-container">
+                {!show ? <button className="btn btn-light btn-lg btn-block" onClick={handleBetClick}>Place Bet</button> : null}
+                  {show ? <BetsForm bet={bet}
+                          handleAddBet={handleAddBet}
+                          handleDeleteBet={handleDeleteBet}
+                  /> : null}
+
+              </div>
+               
+            </figure>
+          </div>
+
+
+
+
+
         </>
     )
 }
 
 export default BetsCard
+
+// {/* <div className="bets-card">  
+
+// <figure className="bets-card-water">
+//   <div class="bets-card-img-container">
+//     <img src={player.image} alt = "hello"/>
+//   </div>
+  
+//   <figcaption class="bets-card-caption">
+//     <h1 class="bets-card-name">{player.name}</h1>
+
+//     {/* <h3 class="card__type">
+//       normal
+//     </h3> */}
+
+//     {/* <table class="card__stats">
+//       <tbody><tr>
+//         <th>HP</th>
+//         <td>55</td>
+//       </tr>
+//       <tr>
+//         <th>Attack</th>
+//         <td>55</td>
+//       </tr>
+      
+//       <tr>
+//         <th>Defense</th>
+//         <td>50</td>
+//       </tr>
+
+//       <tr>
+//         <th>Special Attack</th>
+//         <td>45</td>
+//       </tr>
+//       <tr>
+//         <th>Special Defense</th>
+//         <td>65</td>
+//       </tr>
+//       <tr>
+//         <th>Speed</th>  
+//         <td>55</td>
+//       </tr>
+//     </tbody></table> */}
+    
+//     <div class="bets-card-money">
+//       <h4 class="bets-card-money-child">
+//         <span class="bets-card-label">Odds</span>
+//         {odds}
+//       </h4>
+//       <h4 class="bets-card-money-child">
+//         <span class="bets-card-label">Current Bets</span>
+//         ${current_bets}
+//       </h4>
+//     </div>
+//   </figcaption>
+// </figure> */}
